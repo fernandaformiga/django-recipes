57a09038ba228d0f29a2ee48fa07a06d71071d61
fernandaformiga <ferformigas@gmail.com>
2021-05-24 21:56:52 -0300

    Changes in admin display and filter

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Recipe
 
+class RecipesList(admin.ModelAdmin):
+    list_display = ('id', 'recipe_name')
+    list_display_links = ('id',)
+    search_fields = ('nome_receita',)
+    search_fields = ('category',)
+
 # Register your models here.
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipesList)
