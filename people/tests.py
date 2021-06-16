a60ce794d563b20ce48d7685bcd9b5d4b52ebe20
fernandaformiga <ferformigas@gmail.com>
2021-06-16 17:55:10 -0300

    Write tests for people

--- people/tests.py
+++ people/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from unittest import TestCase, mock
+from .models import Person
 
-# Create your tests here.
+class TestPersonMock(TestCase):
+    # test with mock
+
+    def test_person_mock(self):
+        mock_person = mock.Mock(spec=Person)
+        self.assertIsInstance(mock_person, Person)
+         
+class TestPerson(TestCase):
+    # set up person for testing
+
+    def setUp(self):
+        Person.objects.create(name="José",
+                              age="35",
+                              occupation="restaurant owner")
+        
+    def test_person_is_created(self):
+        person = Person.objects.get(name="José")
+        self.assertEqual(person.occupation, "restaurant owner")
